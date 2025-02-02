4f03c0240faa20e70bd366caa03965b0ea100222
chao79 <chao79@purdue.edu>
2025-02-02 17:14:15 -0500

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,42 +1,57 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
-def histogram(input_dictionary: dict) -> list:
-    # data is a dictionary that contains the following keys: 'data', 'n', 'min_val', 'max_val'
-    # n is an integer
-    # min_val and max_val are floats
-    # data is a list
+def histogram(d: dict) -> list:
+    a, b, c, e = d['data'], d['n'], d['min_val'], d['max_val']
 
-    # Write your code here
+    if c == e:
+        print("Error: min_val and max_val are the same value")
+        return []
 
-    # return the variable storing the histogram
-    # Output should be a list
-    pass
+    if b <= 0:
+        return []
 
-# Here, the function first checks if the lower and upper bounds are the same, 
-# if they are it prints an error message and returns an empty list. 
-# If lower bound is greater than upper bound, it swaps their values. 
-# If number of bins is less than or equal to 0, it returns an empty list. 
-# Then it initializes an empty list hist of length n and calculates the width of each bin. 
-# Then it iterates through the data, 
-# and for each value checks if it is within the range of the histogram and if it is, 
-# it increments the bin it belongs to. Finally, it returns the histogram.
+    if c > e:
+        c, e = e, c
 
-def combine_birthday_data(person_to_day: List[Tuple[str, int]], 
-                          person_to_month: List[Tuple[str, int]], 
-                          person_to_year: List[Tuple[str, int]]) -> dict:
-    #person_to_day, person_to_month, person_to_year are list of tuples
+    f = [0] * b
+    g = (e - c) / b
 
-    # Write your code here
+    for i in a:
+        if c <= i < e:
+            j = min(b - 1, int((i - c) / g))
+            f[j] += 1
 
-    # return the variable storing output
-    # Output should be a dictionary
+    return f
 
-    pass
+def combine_birthday_data(a: List[Tuple[str, int]], 
+                          b: List[Tuple[str, int]], 
+                          c: List[Tuple[str, int]]) -> Dict[int, Tuple]:
+    y, d = 2025, {}
 
-# We first define the current year as 2024, which will be used to calculate the age of each person later on.
-# We create an empty dictionary month_to_people_data that will store the final data in the format specified in the problem statement.
-# We iterate over the both values in the tuple of the person_to_month list (note that person_to_month is a list of tuples, which means each item in the list is a tuple) 
-# using a for loop. For each iteration, we extract the corresponding day, year and age values from the person_to_day and person_to_year lists using the current name as the "key".
-# We then use the current month as the key and a tuple of (name, day, year, age) as the value to update the month_to_people_data dictionary.
-# Only change the value to a list data type, when there are multiple entries with the same key. This will help append for other tuples to the same month.
-# Finally, we return the month_to_people_data dictionary as the output of the function.
+    for i, j in a:
+        d[i] = {'d': j}
+    
+    for i, j in b:
+        if i in d:
+            d[i]['m'] = j
+    
+    for i, j in c:
+        if i in d:
+            d[i]['y'] = j
+            d[i]['a'] = y - j
+    
+    r = {}
+
+    for i, j in d.items():
+        m = j.get('m')
+        if m is not None:
+            t = (i, j['d'], j['y'], j['a'])
+            if m in r:
+                if isinstance(r[m], list):
+                    r[m].append(t)
+                else:
+                    r[m] = [r[m], t]
+            else:
+                r[m] = t
+
+    return r
